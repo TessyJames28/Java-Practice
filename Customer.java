62d85aeec87ec5642bf40c226ba88b1388e82d09
Tessyjames <tessyjames28@gmail.com>
2024-12-22 18:19:55 +0100

    Customer class for customer digital object

---
+++ Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+    private String lastname;
+    private String firstname;
+
+    public String getLastname(){
+        return this.lastname;
+    }
+
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
+
+    public String getFirstname(){
+        return this.firstname;
+    }
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname;
+    }
+
+}
